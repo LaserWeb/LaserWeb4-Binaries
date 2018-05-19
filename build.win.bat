c944dee9fd493e3f536d2fd0bdf81bcadc097bf6
Jorge Robles <jorge.diego.robles@gmail.com>
2018-05-19 13:07:15 +0200

    WIN batch WIP

--- build.win.bat
+++ build.win.bat
@@ -6,6 +6,7 @@
 
 :: Set UnicodeData.txt path to work around https://github.com/dodo/node-unicodetable/issues/16
 set NODE_UNICODETABLE_UNICODEDATA_TXT=%CD%\UnicodeData\UnicodeData.txt
+set LW_DIR=Laserweb4 
 
 :: Set target branch
 set /p TARGET_UI_BRANCH=<BRANCH
@@ -16,15 +17,15 @@ cd ..
 dir
 
 :: Download LaserWeb UI / install modules
-IF NOT EXIST "LaserWeb4" (
-    git clone https://github.com/Laserweb/LaserWeb4.git
-    cd LaserWeb4
+IF NOT EXIST %LW_DIR% (
+    git clone https://github.com/Laserweb/LaserWeb4.git %LW_DIR%
+    cd %LW_DIR%
     git checkout %TARGET_UI_BRANCH%
     CALL yarn
     CALL npm run installdev
 ) ELSE (
     echo "LaserWeb4 folder exists, skip download.."
-    cd LaserWeb4
+    cd %LW_DIR%
 )
 
 :: Override files
@@ -35,23 +36,20 @@ IF NOT EXIST "LaserWeb4" (
 git log --pretty=format:"[%%h](https://github.com/Laserweb/LaserWeb4/commit/%%H)%%x09%%an%%x09%%ad%%x09%%s" --date=short -10 > git.log.output
 set /p GIT_LOGS=<git.log.output
 
+git describe --abbrev=0 --tags > ui_version.output
+set /p UI_VERSION=<ui_version.output
+set /p SERVER_VERSION_FULL=<node_modules\lw.comm-server\version.txt
+set SERVER_VERSION = %SERVER_VERSION_FULL:~-3%
+
 :: Bundle LaserWeb app using webpack
 CALL npm run bundle-dev
-cd ..
+:: Copy web front-end
+cd %CD%
+git tag -f %UI_VERSION%-%$SERVER_VERSION%
+xcopy ..\%LW_DIR%\dist .\app
 
-:: Download LaserWeb server component / install modules
-IF NOT EXIST "lw.comm-server" (
-    git clone https://github.com/Laserweb/lw.comm-server.git
-    cd lw.comm-server
-    git checkout "electron_bundler"
-    CALL yarn
-) ELSE (
-    echo "lw.comm-server folder exists, skip download.."
-    cd lw.comm-server
-)
-
-:: Copy web front-end + build server component
-CALL npm run dist
+.\node_modules\.bin\electron-rebuild
+.\node_modules\.bin\build --em.version=%UI_VERSION%-%$SERVER_VERSION% -p never
 
 :: Move release file to distribution directory
 xcopy dist\*.exe ..\LaserWeb4-Binaries\dist\
